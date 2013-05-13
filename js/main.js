cc3dbe7e80bc51c2a36fbf8e21bf4d9708e7d1ac
Kouki92 <alice.eriksson@hyperisland.se>
2013-05-13 10:01:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 // Why hello there, sexy.
 // This is a javascript comment. Any line that begins with // will be like this.
 // Comments have NO effect on your webpage. NONE! ZERO!
+// OMG! IS IT TRUE?!
